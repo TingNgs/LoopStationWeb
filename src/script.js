9d7ec022be1c5c60c14a208095eb9ef115f7f63b
ngSgn <t105590049@ntut.org.tw>
2018-10-30 20:27:25 +0800

    test ipad

--- src/script.js
+++ src/script.js
@@ -1,43 +1,47 @@
-var stream
-const sleep = time => new Promise(resolve => setTimeout(resolve, time));
-var audio_context;
-var input;
-var analyser;
-//var testLooper = new Looper(audio_context)
+window.onload = function(){
 
-async function RecordTest() {
-    document.getElementById('r').innerText = "sd";
-    recorder = new Recorder(input);
-    recorder.record();
-}
-
-function stopRecording(button) {
-    recorder.stop();
-
-    // create WAV download link using audio data blob
-    createDownloadLink();
-
-    recorder.clear();
-}
-window.onload = async function () {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
     window.URL = window.URL || window.webkitURL;
-    audio_context = new AudioContext;
-    stream = await navigator.mediaDevices.getUserMedia({
-        audio: true
+    var recorder;
+  
+    var s = document.getElementById('s');
+    var p = document.getElementById('p');
+    var timer;
+    var context = new AudioContext();
+  
+    navigator.getUserMedia({audio: true}, function(stream) {
+      var microphone = context.createMediaStreamSource(stream);
+      var analyser = context.createAnalyser();
+      microphone.connect(analyser);
+      //analyser.connect(context.destination);
+      analyser.fftSize = 2048;
+      var dataArray = new Uint8Array(analyser.fftSize);
+      analyser.getByteFrequencyData(dataArray);
+  
+      s.onclick = function(){
+        recorder.stop();
+        createDownloadLink();
+        recorder.clear();
+      };
+  
+      p.onclick = function(){
+        recorder = new Recorder(microphone);
+        recorder.record();
+      };
+  
+      function createDownloadLink(){
+        recorder.exportWAV(function(blob) {
+          var url = URL.createObjectURL(blob);
+          var au = document.createElement('audio');
+          
+          au.controls = true;
+          au.src = url;
+          au.play();
+        });
+      }
+  
+    }, function(){
+      console.log('error');
     });
-    input = audio_context.createMediaStreamSource(stream);
-    analyser = context.createAnalyser();
-    microphone.connect(analyser);
-}
-
-function createDownloadLink() {
-    recorder.exportWAV(function (blob) {
-        var url = URL.createObjectURL(blob);
-        var au = document.createElement('audio');
-        au.controls = true;
-        au.src = url;
-        au.play();
-    });
-}
+    };
