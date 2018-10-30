b3e88646b43ce56c0d29856c9103afd9d7db3304
ngSgn <t105590049@ntut.org.tw>
2018-10-30 20:04:27 +0800

    test ipad

--- src/script.js
+++ src/script.js
@@ -1,35 +1,36 @@
-window.onload = async function () {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-    window.URL = window.URL || window.webkitURL;
 
-    audio_context = new AudioContext;
-    stream = await navigator.mediaDevices.getUserMedia({
-        audio: true
-    });
-}
 var stream
 const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 var audio_context;
+var input;
 //var testLooper = new Looper(audio_context)
 
 async function RecordTest() {
-    var input = audio_context.createMediaStreamSource(stream);
     recorder = new Recorder(input);
-    recorder && recorder.record();
+    recorder.record();
 }
 
 function stopRecording(button) {
-    recorder && recorder.stop();
+    recorder.stop();
 
     // create WAV download link using audio data blob
     createDownloadLink();
 
     recorder.clear();
 }
+window.onload = async function () {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    window.URL = window.URL || window.webkitURL;
+    audio_context = new AudioContext;
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    input = audio_context.createMediaStreamSource(stream);
+}
 
 function createDownloadLink() {
-    recorder && recorder.exportWAV(function (blob) {
+    recorder.exportWAV(function (blob) {
         var url = URL.createObjectURL(blob);
         var au = document.createElement('audio');
         au.controls = true;
