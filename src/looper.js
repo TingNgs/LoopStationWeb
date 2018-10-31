2bc4c875e5a05dc57707da70e3a9d67dd1e69958
ngSgn <t105590049@ntut.org.tw>
2018-10-31 13:40:58 +0800

    test ipad record

--- src/looper.js
+++ src/looper.js
@@ -1,17 +1,36 @@
 class Looper
 {
-    constructor(audio_context)
+    constructor()
     {
-        _audio_context = audio_context;
-        recorderList = [];
+        this.recorded = false;
+        _audio_context = new AudioContext();
+        this.recorderList = [];
+        this.looping = false;
     }
-    startRecord(stream)
+    RecordAudio(stream)
     {
-        var input = audio_context.createMediaStreamSource(stream);
+        console.log("start record");
+        this.recorded = true;
+        var input = _audio_context.createMediaStreamSource(stream);
         _recorder = new Recorder(input)
+        _recorder.record();
+    }
+    async StopReocrd()
+    {
+        var temp_audio = document.createElement("AUDIO");
+        temp_audio.controls = true; 
+        _recorder.stop();
+        _recorder.exportWAV(function(blob) {
+            temp_audio.src = URL.createObjectURL(blob);
+        });
+        temp_audio.play()
+        this.recorderList.push(temp_audio);
+        _recorder.clear();
+        console.log("stop reocrd")
     }
 }
-
+this.recorded;
+this.looping;
 let _recorder;
 let _audio_context;
-let recorderList;
+this.recorderList = [];
--- src/script.js
+++ src/script.js
@@ -1,24 +1,54 @@
-window.onload = function(){
+window.onload = function() {
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  navigator.getUserMedia =
+    navigator.getUserMedia || navigator.webkitGetUserMedia;
+  window.URL = window.URL || window.webkitURL;
+  var recorder;
+  var recordButtonList = [];
+  var LooperList = [];
+  var p = document.getElementById("p");
+  recordButtonList.push(document.getElementById("r1"));
+  recordButtonList.push(document.getElementById("r2"));
+  recordButtonList.push(document.getElementById("r3"));
+  recordButtonList.push(document.getElementById("r4"));
+  recordButtonList.push(document.getElementById("r5"));
+  recordButtonList.push(document.getElementById("r6"));
+  console.log(recordButtonList[5].id);
+  for (var i = 0; i < 6; i++) LooperList.push(new Looper());
+  var context = new AudioContext();
+  var audioList = [];
 
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-    window.URL = window.URL || window.webkitURL;
-    var recorder;
-  
-    var s = document.getElementById('s');
-    var p = document.getElementById('p');
-    var r = document.getElementById('r');
-    var timer;
-    var context = new AudioContext();
-    var audioList = [];
-  
-    navigator.getUserMedia({audio: true}, function(stream) {
+  navigator.getUserMedia(
+    {
+      audio: true
+    },
+    function(stream) {
       var microphone = context.createMediaStreamSource(stream);
-      var analyser = context.createAnalyser();
-  
-      s.onclick = function(){
+
+      for (let i = 0; i < recordButtonList.length; i++)
+        recordButtonList[i].onclick = async function() {
+          let clickedButton;
+          for (let j = 0; j < recordButtonList.length; j++)
+            if (recordButtonList[j].id == this.id) clickedButton = j;
+          if (!LooperList[clickedButton].recorded) {
+            LooperList[clickedButton].RecordAudio(stream);
+            await sleep(3000);
+            await LooperList[clickedButton].StopReocrd();
+          } else {
+            for (
+              let j = 0;
+              j < LooperList[clickedButton].recorderList.length;
+              j++
+            ) {
+              LooperList[clickedButton].recorderList[j].loop = true;
+              LooperList[clickedButton].recorderList[j].play();
+              console.log("play");
+            }
+          }
+        };
+      /*s.onclick = function(){
         var defual_audio = document.createElement("AUDIO");
-    defual_audio.controls = true;  
+        defual_audio.controls = true;  
         recorder.stop();
         recorder.exportWAV(function(blob) {
           defual_audio.src = URL.createObjectURL(blob);
@@ -37,9 +67,11 @@ window.onload = function(){
           audioList[i].play();
       }
       function createDownloadLink(){
-      }
-  
-    }, function(){
-      console.log('error');
-    });
-    };
+      }*/
+    },
+    function() {
+      console.log("error");
+    }
+  );
+};
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
