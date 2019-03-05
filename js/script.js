f47ba9b2d59c681d0ad2d0fd8e54355f6d77e2f3
ngSgn <t105590049@ntut.org.tw>
2019-03-06 02:03:43 +0800

    Add svg main button and color

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-$(function() {
+$(function () {
     $("#main").load("record_control.html");
-    rec = Recorder({ bitRate: 320, sampleRate: 48000 });
+    rec = Recorder({
+        bitRate: 320,
+        sampleRate: 48000
+    });
     rec.open(
-      function() {
-        //Open the mic and grant for recorder
-      },
-      function(msg) {}
+        function () {
+            //Open the mic and grant for recorder
+        },
+        function (msg) {}
     );
-  });
+});
 
 var rec;
 var anyLooping = false;
@@ -19,108 +22,126 @@ var loopFunction; // For setInterval and clear interval
 var looperList = [];
 for (var i = 0; i < 6; i++) looperList.push(new Looper());
 
-function StartLooping(){
-    loopFunction = setInterval(LoopFunction,maxDuration)
+function StartLooping() {
+    loopFunction = setInterval(LoopFunction, maxDuration)
     LoopFunction()
 }
 
-function CheckEndLoop(){
+function CheckEndLoop() {
     var endLoop = true;
     looperList.forEach(element => {
-        if(element.looping) endLoop = false;
+        if (element.looping) endLoop = false;
     });
-    if(endLoop) {
+    if (endLoop) {
         clearInterval(loopFunction);
         anyLooping = false;
         console.log("Stop Loop");
     }
 }
 
-function LoopFunction(){
+function LoopFunction() {
     console.log(new Date().getTime() - loopStartTime);
     loopStartTime = new Date().getTime();
     console.log("Loop starting time : " + loopStartTime);
     looperList.forEach(element => {
-        if(element.looping == true){
-            element.recorderList.forEach(element=>{
+        if (element.looping == true) {
+            element.recorderList.forEach(element => {
                 element.currentTime = 0;
                 element.play();
+                console.log(element);
             });
         }
     });
 }
 
 function OnClickRrecorder(x) {
-  //Main control button
-  if (looperList[x].recorded) {
-    //Recorded, play or stop loop
-    if (looperList[x].looping) {
-        looperList[x].looping = false;
-        console.log("Recorder " +x+ " stop looping");
-        looperList[x].recorderList.forEach(element => {
-            element.pause();
-        });
-        CheckEndLoop();
-    } else {
-        console.log("Recorder " +x+ " start looping");
+    //Main control button
+    if (looperList[x].recorded) {
+        //Recorded, play or stop loop
+        if (looperList[x].looping) {
+            looperList[x].looping = false;
+            console.log("Recorder " + x + " stop looping");
+            ChangeMainButtonState(x, 3);
+            looperList[x].recorderList.forEach(element => {
+                element.pause();
+            });
+            CheckEndLoop();
+        } else {
+            console.log("Recorder " + x + " start looping");
 
-        if(anyLooping){
-            var timeouttTime = maxDuration - (new Date().getTime() - loopStartTime);
-            console.log("Recorder " +x+ " start at "+timeouttTime);
-            setTimeout(looperList[x].looping = true,timeouttTime);
-        }
-        else{
-            looperList[x].looping = true;
-            StartLooping();
+            var timeouttTime = 0;
+            if (anyLooping) timeouttTime = maxDuration - (new Date().getTime() - loopStartTime);
             anyLooping = true;
+            console.log("Recorder " + x + " start at " + timeouttTime);
+            setTimeout(function () {
+                looperList[x].looping = true;
+                ChangeMainButtonState(x, 4);
+            }, timeouttTime);
         }
-    }
-  } else {
-    //Not recorded, ready to record
-    console.log(x);
-    if(recording){
-        rec.stop(
-            function(blob, duration) {
-              var audio = document.createElement("AUDIO");
-              audio.src = URL.createObjectURL(blob);
-              looperList[x].recorderList.push(audio);
-              looperList[x].recorded = true;
-              console.log("New audio duration : " + duration);
-              var newDuration = false;
-              if(duration > maxDuration) {
-                  maxDuration = duration;
-                  newDuration = true;
-              }
-              if(anyLooping){
-                  var timeouttTime = 0;
-                if(!newDuration)
-                    timeouttTime = maxDuration - (new Date().getTime() - loopStartTime);
-                setTimeout(StartLooping,timeouttTime);
-              }
-            },
-            function(msg) {
-              console.log("Fail:" + msg);
+    } else {
+        //Not recorded, ready to record
+        console.log(x);
+        if (recording) {
+            rec.stop(
+                function (blob, duration) {
+                    var audio = document.createElement("AUDIO");
+                    audio.src = URL.createObjectURL(blob);
+                    looperList[x].recorderList.push(audio);
+                    looperList[x].recorded = true;
+                    console.log("New audio duration : " + duration);
+                    var newDuration = false;
+                    if (duration > maxDuration) {
+                        maxDuration = duration;
+                        newDuration = true;
+                    }
+                    if (anyLooping) {
+                        var timeouttTime = 0;
+                        if (!newDuration)
+                            timeouttTime = maxDuration - (new Date().getTime() - loopStartTime);
+                        setTimeout(StartLooping, timeouttTime);
+                    }
+                    ChangeMainButtonState(x, 3);
+                },
+                function (msg) {
+                    console.log("Fail:" + msg);
+                }
+            );
+        } else {
+            var timeouttTime = 1500;
+            ChangeMainButtonState(x, 1);
+            if (anyLooping) {
+                timeouttTime = maxDuration - (new Date().getTime() - loopStartTime);
+                clearInterval(loopFunction);
             }
-        );
-    }
-    else{
-        var timeouttTime = 1500;
-        if(anyLooping){
-            timeouttTime = maxDuration - (new Date().getTime() - loopStartTime);
-            clearInterval(loopFunction);
+            console.log("Record start at : " + timeouttTime);
+            setTimeout(function () {
+                rec.start(); //start recorded
+                ChangeMainButtonState(x, 2);
+                if (anyLooping) LoopFunction();
+            }, timeouttTime);
         }
-        console.log("Record start at : " + timeouttTime);
-        setTimeout(function(){
-            rec.start(); //start recorded
-            if(anyLooping)LoopFunction();
-        },timeouttTime);
+        recording = !recording;
     }
-    recording = !recording;
-  }
 }
 
 function OnClickReset(x) {
-  //Reset button for recorder
-  looperList[x].Reset();
-  CheckEndLoop();
+    //Reset button for recorder
+    looperList[x].Reset();
+    ChangeMainButtonState(x, 0);
+    CheckEndLoop();
+}
+
+function ChangeMainButtonState(x, n) {
+    let id = "recorder" + x;
+    console.log(id);
+    document.getElementById(id).classList.remove("empty");
+    document.getElementById(id).classList.remove("waiting");
+    document.getElementById(id).classList.remove("recording");
+    document.getElementById(id).classList.remove("recorded");
+    document.getElementById(id).classList.remove("looping");
+    if (n == 0) document.getElementById(id).classList.add("empty");
+    if (n == 1) document.getElementById(id).classList.add("waiting");
+    if (n == 2) document.getElementById(id).classList.add("recording");
+    if (n == 3) document.getElementById(id).classList.add("recorded");
+    if (n == 4) document.getElementById(id).classList.add("looping");
 }
